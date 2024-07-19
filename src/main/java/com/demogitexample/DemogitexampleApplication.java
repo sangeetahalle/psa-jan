ed0f0f8abb558576d17f5dfaad801ca9efe1a177
sangeetahalle <sangeetahalle25@gmail.com>
2024-07-19 08:05:31 -0700

    test1

---
+++ src/main/java/com/demogitexample/DemogitexampleApplication.java
@@ -0,0 +1,13 @@
+package com.demogitexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogitexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogitexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demogitexample/DemogitexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demogitexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
